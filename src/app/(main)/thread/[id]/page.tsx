cb8ef575d126f0bbb5a4f8bdc6ec176ad420cc74
jeonbyeongmin <qudals7613@gmail.com>
2023-08-18 13:11:27 +0900

    feat: update ui detail page

--- src/app/(main)/thread/[id]/page.tsx
+++ src/app/(main)/thread/[id]/page.tsx
@@ -1,8 +1,7 @@
 import MainThread from '@/app/(main)/thread/[id]/MainThread';
 import { ReplyThreadList } from '@/app/(main)/thread/[id]/ReplyThreadList';
 import { threadsMock } from '@/app/thread-mock';
-import MovieCard from '@/components/MovieCard';
-import { Flex } from '@/components/base';
+import { Button, Flex, Icon, Text } from '@/components/base';
 import type { Movie } from '@/types/movie';
 
 export default function ThreadDetailPage() {
@@ -20,12 +19,21 @@ export default function ThreadDetailPage() {
   };
 
   return (
-    <Flex direction="col" className="pt-20 flex-1 px-5" align="center" justify="center">
-      <MovieCard movie={movie} />
-      <MainThread thread={threadsMock[0]} />
-      <Flex direction="col">
-        <div className="text-lg">답변</div>
-        <ReplyThreadList threads={threadsMock} />
+    <Flex direction="col" className="mt-16 md:mt-0 flex-1" align="center" justify="center">
+      <Flex className="border-b h-[4rem] border-gray-700 w-full py-3" align="center" gap={2}>
+        <Button variant="ghost" radius="full" className="p-2">
+          <Icon name="arrowLeft" fill="none" size={18} />
+        </Button>
+        <Text weight="bold" size="lg" lineClamp={1} className="flex-1">
+          애브리씽 애브리웨어 올 앳 원스
+        </Text>
+      </Flex>
+      <Flex direction="col" className="m-3">
+        <MainThread thread={threadsMock[0]} />
+        <Flex direction="col">
+          <div className="text-lg">답변</div>
+          <ReplyThreadList threads={threadsMock} />
+        </Flex>
       </Flex>
     </Flex>
   );
