987952831f0315b705865b199df62621fa1abc77
jeonbyeongmin <qudals7613@gmail.com>
2023-08-19 11:00:04 +0900

    feat: update fetcher

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,10 +1,10 @@
 /**
  * 커스텀 패치를 위한 래퍼 함수
- * @param {string} endpoint
- * @param param1
+ * @param endpoint
+ * @param config
  * @returns {Promise<T>}
  */
-export async function fetcher<T>(endpoint: string, { method = 'GET', body, ...customConfig }: RequestInit): Promise<T> {
+export async function fetcher<T>(endpoint: string, { body, ...customConfig }: RequestInit): Promise<T> {
   const headers: RequestInit['headers'] = {};
 
   if (body) {
@@ -12,23 +12,20 @@ export async function fetcher<T>(endpoint: string, { method = 'GET', body, ...cu
   }
 
   const config: RequestInit = {
-    method,
     headers,
     body: body ? JSON.stringify(body) : undefined,
     ...customConfig,
   };
 
-  // Remove leading slash
   if (endpoint.startsWith('/')) {
     endpoint = endpoint.slice(1);
   }
 
   try {
     const response = await fetch(`/api/${endpoint}`, config);
-
     const data = await response.json();
 
-    return response.status < 400 ? data : Promise.reject(data);
+    return response.ok ? data : Promise.reject(data);
   } catch (error) {
     return Promise.reject(error);
   }
