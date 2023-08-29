a975f49adeb9897f9185f40797bc2f9517b2b2f6
jeonbyeongmin <qudals7613@gmail.com>
2023-08-29 21:53:40 +0900

    build: install storybook

--- src/components/base/Button/index.tsx
+++ src/components/base/Button/index.tsx
@@ -1,20 +1,2 @@
-import { type ButtonVariants } from '@/components/base/button/button.recipe';
-import { type ColorPalette } from '@/styled-system/tokens';
-import { forwardRef, type ComponentPropsWithoutRef } from 'react';
-
-interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
-  colorPalette?: ColorPalette;
-  variants?: ButtonVariants;
-}
-
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-  const { variants, children, title, className, ...rest } = props;
-
-  return (
-    <button ref={ref} {...rest}>
-      {children}
-    </button>
-  );
-});
-
-Button.displayName = 'Button';
+export * from './button';
+export * from './button.recipe';
