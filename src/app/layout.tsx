21e52659d4d2756126f9b47502644e272a3b11fd
jeonbyeongmin <qudals7613@gmail.com>
2023-08-29 17:03:40 +0900

    build: activate pandacss jsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { NavBar } from '@/components/NavBar';
-import { Flex } from '@/components/base';
 import { QueryClientProvider } from '@/lib/query/provider';
 import { MSWContainer } from '@/mocks/MockContainer';
+import { Stack } from '@/styled-system/jsx';
 import type { Metadata } from 'next';
 import { Noto_Sans } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
@@ -33,13 +33,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <MSWContainer>
           <QueryClientProvider>
             <NextTopLoader color="#F4F4F5" showSpinner={false} height={1} shadow={false} />
-            <Flex className="w-full h-full" justify="center">
-              <Flex className="max-w-screen-xl w-full h-full relative">
+            <Stack className="w-full h-full" justify="center">
+              <Stack className="max-w-screen-xl w-full h-full relative">
                 <NavBar />
                 <main className="flex-1 h-full md:ml-56">{children}</main>
                 <div className="w-80 hidden lg:block bg-gray-950 h-[100vh] sticky top-0" />
-              </Flex>
-            </Flex>
+              </Stack>
+            </Stack>
           </QueryClientProvider>
         </MSWContainer>
       </body>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import MovieSelectModal from '@/components/MovieSelectModal';
-import { Avatar, Button, Flex, Icon, Logo, Text } from '@/components/base';
+import { Avatar, Button, Icon, Logo, Text } from '@/components/base';
+import { Stack } from '@/styled-system/jsx';
 import classNames from 'classnames';
 import Link from 'next/link';
 import { useCallback, useEffect, useState } from 'react';
@@ -40,8 +41,7 @@ export default function Header() {
   }, [controlNavbar, lastScrollY]);
 
   return (
-    <Flex
-      as="header"
+    <Stack
       className={classNames(
         'h-16 w-full fixed px-5 z-20 bg-gray-950 md:hidden transition-all duration-300 border-b border-gray-800',
         {
@@ -52,35 +52,35 @@ export default function Header() {
       align="center"
       justify="center"
     >
-      <Flex direction="row" align="center" justify="between" className="max-w-screen-xl w-full">
-        <Flex direction="row" align="center">
+      <Stack direction="row" align="center" justify="between" className="max-w-screen-xl w-full">
+        <Stack direction="row" align="center">
           <Button className="p-1" variant="ghost">
             <Icon name="menu" size={20} />
           </Button>
           <Link href="/" className="px-3">
             <Logo width={100} height={30} />
           </Link>
-        </Flex>
+        </Stack>
         {loggedIn ? (
-          <Flex direction="row" align="stretch" gap={2}>
-            <Button variant="solid" className="flex-1 px-5" radius="full" onClick={openModal}>
+          <Stack direction="row" align="stretch" gap={2}>
+            <Button variant="solid" className="stackStack-1 px-5" radius="full" onClick={openModal}>
               <Text size="sm" weight="medium">
                 글 쓰기
               </Text>
             </Button>
             <Avatar src="https://avatars.githubusercontent.com/u/48426991?v=4" size="sm" />
-          </Flex>
+          </Stack>
         ) : (
-          <Flex direction="row" align="center" gap={1}>
+          <Stack direction="row" align="center" gap={1}>
             <Link href="/login">
               <Button variant="solid" className="px-3 py-2">
                 로그인
               </Button>
             </Link>
-          </Flex>
+          </Stack>
         )}
-      </Flex>
+      </Stack>
       <MovieSelectModal isOpen={isModalOpen} closeModal={closeModal} />
-    </Flex>
+    </Stack>
   );
 }
