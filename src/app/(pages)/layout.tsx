ebc45873ae86166638d92e5870782d05aa10f043
jeonbyeongmin <qudals7613@gmail.com>
2023-09-04 22:46:28 +0900

    feat: 모달에 Parallel Routes 활용

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,4 +1,4 @@
-import { GlobalModal, Navbar } from '@/app/(pages)/components';
+import { Sidebar } from '@/components';
 import { css } from '@/styled-system/css';
 import { Container } from '@/styled-system/jsx';
 import { token } from '@/styled-system/tokens';
@@ -10,18 +10,23 @@ export const metadata: Metadata = {
   description: '영화를 좋아하는 사람들을 위한 커뮤니티',
 };
 
+interface PagesLayoutProps {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}
+
 /**
  * @description
  * - `PagesLayout` 은 모든 페이지에 공통으로 적용되는 레이아웃입니다.
  * - 모든 페이지에 공통 레이아웃 UI 을 적용합니다.
  */
-export default function PagesLayout({ children }: { children: React.ReactNode }) {
+export default function PagesLayout({ children, modal }: PagesLayoutProps) {
   return (
     <>
       <NextTopLoader color={token('colors.gray.50')} showSpinner={false} height={1} shadow={false} />
-      <GlobalModal />
       <Container paddingX={0} maxW="7xl" css={{ flex: 1, h: 'full', minH: 0 }}>
-        <Navbar />
+        {modal}
+        <Sidebar />
         <main className={css({ flex: 1, h: 'full', ml: { base: 0, md: 64 } })}>{children}</main>
       </Container>
     </>
