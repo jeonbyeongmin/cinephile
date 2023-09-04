33b3aba2306ad8091bbfec359ae7779151f41d89
jeonbyeongmin <qudals7613@gmail.com>
2023-09-05 00:55:18 +0900

    refactor: dialog content 기본 스타일 적용

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Button } from '@/components/button';
 import { Icon } from '@/components/icon';
-import { css } from '@/styled-system/css';
+import { IconButton } from '@/components/icon-button';
+import { css, cx } from '@/styled-system/css';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { forwardRef } from 'react';
 
@@ -10,31 +10,52 @@ interface DialogContentProps extends DialogPrimitive.DialogContentProps {
   children: React.ReactNode;
 }
 
-export const DialogContent = forwardRef<HTMLDivElement, DialogContentProps>(({ children, ...props }, ref) => (
-  <DialogPrimitive.Portal>
-    <DialogPrimitive.Overlay
-      className={css({
-        inset: 0,
-        position: 'fixed',
-        display: 'grid',
-        placeItems: 'center',
-        animation: 'overlay 150ms forwards',
-        p: 5,
-        overflowY: 'auto',
-        zIndex: 1,
-      })}
-    >
-      <DialogPrimitive.Content {...props} ref={ref}>
-        {children}
-        <DialogPrimitive.Close aria-label="Close" asChild>
-          <Button variant="ghost" p={1} css={{ position: 'absolute', top: 5, right: 5 }}>
-            <Icon name="close" size={18} />
-          </Button>
-        </DialogPrimitive.Close>
-      </DialogPrimitive.Content>
-    </DialogPrimitive.Overlay>
-  </DialogPrimitive.Portal>
-));
+export const DialogContent = forwardRef<HTMLDivElement, DialogContentProps>(
+  ({ children, className, ...props }, ref) => (
+    <DialogPrimitive.Portal>
+      <DialogPrimitive.Overlay
+        className={css({
+          inset: 0,
+          position: 'fixed',
+          display: 'grid',
+          placeItems: 'center',
+          animation: 'overlay 150ms forwards',
+          p: 5,
+          overflowY: 'auto',
+          zIndex: 1,
+        })}
+      >
+        <DialogPrimitive.Content
+          className={cx(
+            css({
+              w: 'full',
+              rounded: '2xl',
+              bg: 'gray.950',
+              animation: 'contentShow 150ms forwards',
+              display: 'flex',
+              flexDirection: 'column',
+            }),
+            className
+          )}
+          {...props}
+          ref={ref}
+        >
+          {children}
+          <DialogPrimitive.Close aria-label="Close" asChild>
+            <IconButton
+              aria-label="close"
+              icon={<Icon name="close" size={16} />}
+              size="sm"
+              variant="ghost"
+              rounded="md"
+              css={{ position: 'absolute', top: 3, right: 3 }}
+            />
+          </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+      </DialogPrimitive.Overlay>
+    </DialogPrimitive.Portal>
+  )
+);
 
 DialogContent.displayName = 'DialogContent';
 
