c947d133886bba610a5f885bf564e9c7673af8dc
jeonbyeongmin <qudals7613@gmail.com>
2023-09-07 19:28:37 +0900

    feat: 단일 스레드 페이지 헤더 position 버그, 스레드 개수 추가

--- src/app/(pages)/thread/[id]/components/thread-buttons.tsx
+++ src/app/(pages)/thread/[id]/components/thread-buttons.tsx
@@ -11,13 +11,6 @@ export function ThreadButtons() {
         rounded="full"
       />
       <IconButton
-        icon={<Icon name="reply" fill="none" size={18} />}
-        aria-label="reply button"
-        size="sm"
-        variant="ghost"
-        rounded="full"
-      />
-      <IconButton
         icon={<Icon name="share" fill="none" size={18} />}
         aria-label="share button"
         size="sm"
