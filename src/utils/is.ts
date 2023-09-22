5694a1c55b87d4af30c2906fb9dcc94c24cd9c72
jeonbyeongmin <qudals7613@gmail.com>
2023-09-23 00:26:36 +0900

    fix: MSW가 `isProduction`이 아니고 `isMockEnabled`일 때에만 실행되도록 수정

---
+++ src/utils/is.ts
@@ -0,0 +1,9 @@
+export const isProduction = process.env.NODE_ENV === 'production';
+export const isDevelopment = process.env.NODE_ENV === 'development';
+export const isTest = process.env.NODE_ENV === 'test';
+
+export const isClient = typeof window !== 'undefined';
+export const isServer = !isClient;
+
+export const isMockEnabled = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
+export const isMockDisabled = !isMockEnabled;
