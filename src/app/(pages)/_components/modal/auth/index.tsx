06ad9f59282fa165de065608fe0399c3c9759b8f
jeonbyeongmin <qudals7613@gmail.com>
2023-09-23 22:52:25 +0900

    feat: login modal ui 변경

--- src/app/(pages)/_components/modal/auth/index.tsx
+++ src/app/(pages)/_components/modal/auth/index.tsx
@@ -4,7 +4,7 @@ import { Dialog, DialogContent, Logo } from '@/components';
 import { close, selectModal } from '@/redux/features/modal-slice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { css } from '@/styled-system/css';
-import { center, divider } from '@/styled-system/patterns';
+import { divider, flex } from '@/styled-system/patterns';
 
 import { token } from '@/styled-system/tokens';
 import { Providers } from './providers';
@@ -17,10 +17,12 @@ export function AuthModal() {
 
   return (
     <Dialog open={open} onOpenChange={() => dispatch(close())}>
-      <DialogContent className={center({ maxW: 'md', p: 10 })}>
-        <Logo size={50} color={token('colors.white')} />
-        <p className={css({ mt: 5, fontSize: { base: 'sm', md: 'md' } })}>로그인을 해서 이야기를 시작하세요!</p>
-        <div className={divider({ color: 'gray.600', my: 6 })} />
+      <DialogContent className={css({ maxW: 'md', px: 10, pt: 20, pb: 10 })}>
+        <div className={flex({ direction: 'column', align: 'center' })}>
+          <Logo size={50} color={token('colors.white')} />
+          <p className={css({ mt: 5, fontSize: { base: 'sm', md: 'md' } })}>로그인을 해서 이야기를 시작하세요!</p>
+        </div>
+        <div className={divider({ color: 'gray.800', my: 6 })} />
         <Providers />
       </DialogContent>
     </Dialog>
