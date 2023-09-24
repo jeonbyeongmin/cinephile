facdcc11bc304cc9fccaeda66a68abb0813ef4d0
jeonbyeongmin <qudals7613@gmail.com>
2023-09-24 17:52:09 +0900

    feat: sorter ui 변경

--- src/app/(pages)/home/layout.tsx
+++ src/app/(pages)/home/layout.tsx
@@ -1,11 +1,23 @@
-import { Header } from '@/components';
+import { Button, Header, Icon } from '@/components';
 import { css } from '@/styled-system/css';
 import { Flex } from '@/styled-system/jsx';
+import { flex } from '@/styled-system/patterns';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <Flex direction="column" className={css({ pb: 28 })}>
-      <Header>홈</Header>
+      <Header className={flex({ justify: 'space-between' })}>
+        <span>홈</span>
+        <Button
+          variant="outline"
+          size="sm"
+          css={{ fontSize: 'sm' }}
+          rounded="full"
+          leftElement={<Icon name="sort" size={20} />}
+        >
+          반응순
+        </Button>
+      </Header>
       {children}
     </Flex>
   );
--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,12 +1,7 @@
-import { HomeThreadList, Sorter } from '@/app/(pages)/home/_components';
+import { HomeThreadList } from '@/app/(pages)/home/_components';
 
 export default function HomePage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   const mode = searchParams.sort as 'new' | 'hot';
 
-  return (
-    <>
-      <Sorter mode={mode} />
-      <HomeThreadList type={mode} />
-    </>
-  );
+  return <HomeThreadList type={mode} />;
 }
