b15fe54895b427b8d306284cb364b889ff0e66e5
jeonbyeongmin <qudals7613@gmail.com>
2023-09-30 15:57:42 +0900

    feat: header 수정

--- src/app/(pages)/channel/[id]/_components/header.tsx
+++ src/app/(pages)/channel/[id]/_components/header.tsx
@@ -1,15 +1,34 @@
 'use client';
 
 import { Header } from '@/components';
+import { useBoolean } from '@/hooks';
 import { css } from '@/styled-system/css';
+import { useEffect } from 'react';
 
 interface ChannelDetailHeaderProps {
   title: string;
 }
 
 export function ChannelDetailHeader({ title }: ChannelDetailHeaderProps) {
+  const [transparent, setTransparentTrue, setTransparentFalse] = useBoolean(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setTransparentFalse();
+      } else {
+        setTransparentTrue();
+      }
+    };
+
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [setTransparentFalse, setTransparentTrue]);
+
   return (
-    <Header variant="transparent" className={css({ mb: -14 })}>
+    <Header variant={transparent ? 'transparent' : 'glass'} className={css({ mb: -14, transition: 'all 150ms' })}>
       <Header.Back />
       <Header.Content className={css({ fontSize: 'lg', fontWeight: 'bold' })}>{title}</Header.Content>
     </Header>
