581f0ff2b28ba7e90cc62a548238ee1b5a883c62
Byeongmin Jeon <qudals7613@gmail.com>
2023-10-11 14:20:38 +0900

    fix: 작성된 content가 비었다면 에러처리

--- src/app/(pages)/write/_components/write-editor/index.tsx
+++ src/app/(pages)/write/_components/write-editor/index.tsx
@@ -5,36 +5,38 @@ import StarterKit from '@tiptap/starter-kit';
 
 import { createThread } from '@/api/threads/create-thread';
 import { Button, EditorContent, EditorToolbar, Spinner } from '@/components';
-import { css } from '@/styled-system/css';
+import { css, cx } from '@/styled-system/css';
 import { Flex } from '@/styled-system/jsx';
 import { useMutation } from '@tanstack/react-query';
 import { useEditor } from '@tiptap/react';
 import { useRouter } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
+import { flex } from '@/styled-system/patterns';
 import { WriteEditorTitle } from './write-editor-title';
 
 interface EditorProps {
   channelId: string;
 }
 
+const extensions = [StarterKit, Placeholder.configure({ placeholder: '내용을 입력해주세요' })];
+
 export function WriteEditor({ channelId }: EditorProps) {
   const router = useRouter();
 
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
+  const [error, setError] = useState<string | null>(null);
 
-  const handleContentChange = useCallback((value: string) => {
-    setContent(value);
-  }, []);
-
-  const editor = useEditor({
+  const currentEditor = useEditor({
     content,
-    extensions: [StarterKit, Placeholder.configure({ placeholder: '내용을 입력해주세요' })],
-    onUpdate: ({ editor }) => {
-      handleContentChange(editor?.getHTML() ?? '');
-    },
+    extensions,
     autofocus: true,
+    onUpdate: ({ editor }) => {
+      const isValid = editor.getText().trim() !== '';
+      const content = isValid ? editor.getHTML() : '';
+      setContent(content);
+    },
   });
 
   const { isLoading, mutate } = useMutation({
@@ -42,25 +44,33 @@ export function WriteEditor({ channelId }: EditorProps) {
     onSuccess: () => {
       router.push(`/home?sort=hot`);
     },
+    onError: (error: Error) => {
+      setError(error.message);
+    },
   });
 
   return (
     <Flex direction="column" className={css({ w: 'full', h: 'full', minH: 0, flex: 1 })}>
-      <EditorToolbar editor={editor} />
+      <EditorToolbar editor={currentEditor} />
       <WriteEditorTitle title={title} handleTitleChange={setTitle} />
-      <EditorContent editor={editor} className={css({ px: 3, overflowY: 'auto' })} />
-      <Button
-        mt={3}
-        mx={3}
-        mb={{ base: 20, md: 3 }}
-        rounded="full"
-        alignSelf="end"
-        fontSize={{ base: 'sm', md: 'md' }}
-        minW={{ base: '24', md: '28' }}
-        onClick={() => mutate()}
+      <EditorContent editor={currentEditor} className={css({ px: 3, overflowY: 'auto' })} />
+      <div
+        className={cx(
+          flex({ alignSelf: 'end', alignItems: 'center', gap: 3 }),
+          css({ mt: 3, mx: 3, mb: { base: 20, md: 3 } })
+        )}
       >
-        {isLoading ? <Spinner size="xs" /> : '작성하기'}
-      </Button>
+        <span className={css({ color: 'red.500', fontSize: 'sm', ml: 3 })}>{error}</span>
+        <Button
+          rounded="full"
+          fontSize={{ base: 'sm', md: 'md' }}
+          minW={{ base: '24', md: '28' }}
+          h={{ base: '10', md: '11' }}
+          onClick={() => mutate()}
+        >
+          {isLoading ? <Spinner size="xs" /> : '작성하기'}
+        </Button>
+      </div>
     </Flex>
   );
 }
