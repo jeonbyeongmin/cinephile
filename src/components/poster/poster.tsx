a6562f7ac1ac76c3eb61d133e86f2ff9cbffed1a
jeonbyeongmin <qudals7613@gmail.com>
2023-10-12 22:12:26 +0900

    feat: modal escape 키 버튼으로 close

--- src/components/poster/poster.tsx
+++ src/components/poster/poster.tsx
@@ -1,19 +1,17 @@
 import { css, cx } from '@/styled-system/css';
 import { aspectRatio } from '@/styled-system/patterns';
-import { RadiusToken } from '@/styled-system/tokens';
+import { RadiusToken, SizeToken } from '@/styled-system/tokens';
 import Image, { ImageProps } from 'next/image';
 
 interface PosterProps extends Omit<ImageProps, 'width'> {
   className?: string;
   rounded?: RadiusToken;
-  width?: string;
+  width?: SizeToken | string;
 }
 
 export function Poster({ src, alt, className, width, sizes, rounded = 'md', ...rest }: PosterProps) {
   return (
-    <div
-      className={cx(aspectRatio({ ratio: 11 / 16 }), css({ width, position: 'relative', overflow: 'hidden', rounded }))}
-    >
+    <div className={cx(aspectRatio({ ratio: 11 / 16 }), css({ width, rounded }))}>
       <Image
         src={src}
         alt={alt}
